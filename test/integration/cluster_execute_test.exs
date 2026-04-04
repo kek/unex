20bd9006876df0092aa72867568fe1b2dea64100
Karl Eklund <kalle@karleklund.se>
2026-04-05 01:52:36 +0200

    Add end-to-end compile-cache-sync-execute cluster test

---
+++ test/integration/cluster_execute_test.exs
@@ -0,0 +1,71 @@
+defmodule Uniops.Integration.ClusterExecuteTest do
+  use ExUnit.Case, async: false
+
+  alias Uniops.Cluster.HashCache
+  alias Uniops.Cluster.SyncServer
+
+  @moduletag timeout: 300_000
+
+  setup_all do
+    unless Node.alive?() do
+      {:ok, _} = :net_kernel.start([:uniops_test, :shortnames])
+    end
+
+    :ok
+  end
+
+  setup do
+    pa_args = Enum.flat_map(:code.get_path(), fn p -> [~c"-pa", p] end)
+    {:ok, pid, peer_node} = :peer.start_link(%{name: :uniops_exec_peer, args: pa_args})
+
+    {:ok, _} = :rpc.call(peer_node, Application, :ensure_all_started, [:crypto])
+    {:ok, _} = :rpc.call(peer_node, GenServer, :start, [HashCache, HashCache, [name: HashCache]])
+    {:ok, _} = :rpc.call(peer_node, GenServer, :start, [SyncServer, %{cache: HashCache}, [name: SyncServer]])
+
+    on_exit(fn ->
+      try do
+        :peer.stop(pid)
+      catch
+        _, _ -> :ok
+      end
+    end)
+
+    %{peer: peer_node}
+  end
+
+  test "compile, cache, sync to peer, and execute bytecode", %{peer: peer} do
+    # 1. Compile a simple Unison program
+    source = "main : '{IO, Exception} ()\nmain = do printLine \"synced-and-executed\""
+
+    dir =
+      Path.join(
+        System.tmp_dir!(),
+        "uniops_cluster_exec_#{System.unique_integer([:positive])}"
+      )
+
+    {:ok, workspace} = Uniops.Workspace.create(dir)
+    {:ok, file_path} = Uniops.Workspace.write_source(workspace, "program.u", source)
+    {:ok, uc_path} = Uniops.Compiler.compile(workspace, file_path, "main", "program")
+
+    # 2. Cache the .uc bytes locally
+    uc_bytes = File.read!(uc_path)
+    hash = HashCache.put(uc_bytes)
+
+    # 3. Peer resolves the hash (fetches bytecode from us)
+    assert {:ok, _resolved} = :rpc.call(peer, SyncServer, :resolve, [SyncServer, [hash]])
+
+    # 4. Get synced bytes from peer, write to temp file, execute
+    {:ok, synced_bytes} = :rpc.call(peer, HashCache, :get, [HashCache, hash])
+    assert synced_bytes == uc_bytes
+
+    peer_uc_path = Path.join(System.tmp_dir!(), "synced_#{hash}.uc")
+    File.write!(peer_uc_path, synced_bytes)
+
+    {:ok, result} = Uniops.Runner.run_compiled(peer_uc_path)
+    assert result.stdout =~ "synced-and-executed"
+
+    # 5. Cleanup
+    File.rm(peer_uc_path)
+    Uniops.Workspace.destroy(workspace)
+  end
+end
