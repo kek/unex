5314f2c5600bd3a7f8b441e79d196ac3c440ffb4
Karl Eklund <kalle@karleklund.se>
2026-04-05 01:44:34 +0200

    Add HashCache and SyncServer to supervision tree

--- lib/uniops/application.ex
+++ lib/uniops/application.ex
@@ -1,4 +1,5 @@
 defmodule Uniops.Application do
+  @moduledoc false
   use Application
 
   @impl true
@@ -6,14 +7,23 @@ defmodule Uniops.Application do
     mnesia_dir = Application.get_env(:uniops, :mnesia_dir)
     if mnesia_dir, do: Uniops.Storage.Schema.init(mnesia_dir)
 
-    children =
-      if Application.get_env(:uniops, :start_api, false) do
-        port = Application.get_env(:uniops, :api_port, 4040)
-        [{Bandit, plug: Uniops.API.Router, port: port}]
-      else
-        []
-      end
-
+    children = cluster_children() ++ api_children()
     Supervisor.start_link(children, strategy: :one_for_one, name: Uniops.Supervisor)
+  end
+
+  defp cluster_children do
+    [
+      Uniops.Cluster.HashCache,
+      Uniops.Cluster.SyncServer
+    ]
+  end
+
+  defp api_children do
+    if Application.get_env(:uniops, :start_api, false) do
+      port = Application.get_env(:uniops, :api_port, 4040)
+      [{Bandit, plug: Uniops.API.Router, port: port}]
+    else
+      []
+    end
   end
 end
