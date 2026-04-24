986cf45703b8fb5de2616c2c178f789634fc8d90
Karl Eklund <localpart@gmail.com>
2026-04-24 12:11:31 +0200

    Dashboard: smoke test router + basic auth end-to-end

---
+++ test/unex_dashboard/router_test.exs
@@ -0,0 +1,31 @@
+defmodule Unex.Dashboard.RouterTest do
+  use ExUnit.Case, async: false
+  import Plug.Test
+  import Plug.Conn
+
+  setup do
+    Application.put_env(:unex, :dashboard_username, "admin")
+    Application.put_env(:unex, :dashboard_password, "unex")
+
+    start_supervised!(Unex.Dashboard.Endpoint)
+    :ok
+  end
+
+  test "unauthenticated request returns 401" do
+    conn = Phoenix.ConnTest.build_conn(:get, "/")
+    conn = Unex.Dashboard.Endpoint.call(conn, Unex.Dashboard.Endpoint.init([]))
+    assert conn.status == 401
+  end
+
+  test "authenticated request renders index" do
+    creds = Base.encode64("admin:unex")
+
+    conn =
+      Phoenix.ConnTest.build_conn(:get, "/")
+      |> put_req_header("authorization", "Basic #{creds}")
+
+    conn = Unex.Dashboard.Endpoint.call(conn, Unex.Dashboard.Endpoint.init([]))
+    assert conn.status == 200
+    assert conn.resp_body =~ "Unex Dashboard"
+  end
+end
